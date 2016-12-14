e24027b4e26e7f792c9f873040bd9fdf88debb74
Jess McFalls <jamcfalls11@gmail.com>
2016-12-14 14:09:24 -0500

    Final

--- FinalWebSite/functional_tests.py
+++ FinalWebSite/functional_tests.py
@@ -1,6 +1,5 @@
 # Boiler Plate
 
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -9,23 +8,31 @@ import os
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		if os.name=='nt':
-			self.browser = webdriver.Chrome()
-		else:
-			self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+    def setUp(self):
+        if os.name=='nt':
+            self.browser = webdriver.Chrome()
+        else:
+            self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_home_page(self):
-		self.browser.get('http://localhost:8000/index.html')
+    def test_home_page(self):
+        self.browser.get('http://localhost:8000/index.html')
+        self.assertIn("The Mandelbrot Set",self.browser.title)
+        a=self.browser.find_element_by_tag_name('h1')
+        m=self.browser.find_element_by_tag_name('img')
+        self.assertIn('M.jpg',m.get_attribute('src'))
 
-		# when we are on the home page, the page title says "The Mandelbrot Set"
-		self.assertIn("The Mandelbrot Set",self.browser.title)
-		
+        a=self.browser.find_element_by_id('code')
+        a.click()
+
+        h=self.browser.find_element_by_tag_name('h1')
+        self.assertIn("The Code",h.text)
+        m=self.browser.find_element_by_tag_name('img')
+        self.assertIn('mbrot.png',m.get_attribute('src'))
+
 
 if __name__=="__main__":
-		unittest.main(warnings="ignore")
-
+        unittest.main(warnings="ignore")
